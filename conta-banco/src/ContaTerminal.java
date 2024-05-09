9f6a1a27d3ba8276342daf49c60568a7e2826a34
isJvDev <jv.eds@hotmail.com>
2024-05-09 01:48:56 -0300

    adicionando o projeto da conta-banco.

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Bem vindo ao Banco JAVA ! Por favor, digite o seu primeiro nome:");
+        String primeiroNome = scanner.next();
+
+        System.out.println("Agora o segundo nome:");
+        String segundoNome = scanner.next();
+        
+        System.out.println("Digite a sua agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o número da sua agência:");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite o valor que deseja depositar:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println(textoFinal(primeiroNome,segundoNome,agencia,numero,saldo));
+    }
+
+    public static String textoFinal (String primeiroNome, String segundoNome, String agencia, int numero, double saldo){
+        return "Olá, " + primeiroNome.concat(" ").concat(segundoNome).concat(" obrigado por criar uma conta em nosso banco, sua agência é ").concat(agencia).concat(", e o numero da conta ") + numero + " e o seu saldo de " + saldo + " já está disponível para saque.";
+
+    }
+
+
+}
